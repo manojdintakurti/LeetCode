46c8766369d519a0965561813c9a3d5ca5b37c1b
Manoj Dintakurti <47806460+manojdintakurti@users.noreply.github.com>
2025-05-30 17:46:22 -0400

    Time: 0 ms (100%), Space: 17.8 MB (71.46%) - LeetArchive

---
+++ LeetCode/0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        def strToInt(s):
+            num = 0
+            for i in range(len(s)):
+                num =num*10+ (ord(s[i])-ord('0'))
+            return num
+        if num1 + num2 == 0 :
+            return '0'
+        num1=strToInt(num1)
+        num2=strToInt(num2)
+        return str(num1*num2)
